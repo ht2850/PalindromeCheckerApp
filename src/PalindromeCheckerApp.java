b2f2c92128fd2ec372d6d688e9822c8e27ae521f
ht2850 <ht2850@srmist.edu.in>
2026-02-23 10:33:56 +0530

    UC2: Reversed string technique

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,13 @@
-//public class PalindromeCheckerApp {
-//    public static void main (String[] args) {
-//        System.out.println("Welcome to the Palindrome Checker Management System");
-//        System.out.println("Version : 1.0");
-//        System.out.println("System initialized successfully.");
-//    }
-//}
-
 public class PalindromeCheckerApp {
     public static void main (String[] args) {
         String input = "madam";
+        String reversed = "";
 
         System.out.println("Input text : " + input);
 
-        Boolean isPalindrome = true;
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length()-1-i)) {
-                isPalindrome = false;
-                break;
-            }
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
         }
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        System.out.println("Is it a Palindrome? : " + reversed.equals(input));
     }
 }
