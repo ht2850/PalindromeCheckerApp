1154b4f6c80543142af0074dc05bc60f71289c51
Harsh <ht2850@srmist.edu.in>
2026-03-02 10:26:26 +0530

    UC9: Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,69 +1,23 @@
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "Racecar";
+        String input = "madam";
         String cleanStr = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        if (cleanStr.isEmpty()) {
-            System.out.println("Input : " + input);
-            System.out.println("Is Palindrome? : true");
-            return;
-        }
-
-        Node head = null;
-        Node tail = null;
-        for (char ch : cleanStr.toCharArray()) {
-            Node newNode = new Node(ch);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        Node slow = head;
-        Node fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-
-
-        Node prev = null;
-        Node current = slow;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-
-        Node left = head;
-        Node right = prev;
-        boolean isPalindrome = true;
-
-        while (right != null) {
-            if (left.data != right.data) {
-                isPalindrome = false;
-                break;
-            }
-            left = left.next;
-            right = right.next;
-        }
+        boolean isPalindrome = checkRecursive(cleanStr, 0, cleanStr.length() - 1);
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    public static boolean checkRecursive(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return checkRecursive(str, start + 1, end - 1);
     }
 }
